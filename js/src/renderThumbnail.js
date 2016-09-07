ad386e0929ec51e9981924598c4c6f12e74af3d7
John M Calhoun <johnmcalhoun123@gmail.com>
2016-09-06 20:20:17 -0500

    set some things up for isotope

--- js/src/renderThumbnail.js
+++ js/src/renderThumbnail.js
@@ -2,3 +2,17 @@ goog.provide('bakehard.renderThumbnail');
 
 goog.require('bakehard.templates');
 
+bakehard.renderThumbnail.render=function(){
+    //json to html
+}
+bakehard.renderThumbnail.append=function(){
+    //take list of json
+    //render each json
+    //append to grid
+}
+bakehard.renderThumbnail.load=function(){
+    //calculate page
+    //load in new json
+    //call append
+}
+
