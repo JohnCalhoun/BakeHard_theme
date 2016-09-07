e58f13216e87f25e0dc8f40a3333b8daabf46390
John M Calhoun <johnmcalhoun123@gmail.com>
2016-09-07 18:01:23 -0500

    added img_url and category_string fields to post api

--- php/functions.php
+++ php/functions.php
@@ -25,6 +25,47 @@ function theme_script() {
     wp_enqueue_script("materialize");
     wp_enqueue_script("isotope");
     wp_enqueue_script("bake-hard");
-}
+};
 add_action("wp_enqueue_scripts","theme_script");
+add_action( 'rest_api_init', 'api_register_img_url' );
+function api_register_img_url() {
+    register_rest_field( 'post',
+        'img_url',
+        array(
+            'get_callback'    => 'api_get_img_url',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+};
+function api_get_img_url( $object, $field_name, $request ) {
+    return(get_the_post_thumbnail_url( $object[ 'id' ]) );
+};
+
+add_action( 'rest_api_init', 'api_register_category_string' );
+function api_register_category_string() {
+    register_rest_field( 'post',
+        'category_string',
+        array(
+            'get_callback'    => 'api_get_category_string',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+};
+function api_get_category_string( $object, $field_name, $request ) {
+    $cats=get_the_category(  $object[ 'id' ]);
+    $output=array();
+    foreach($cats as $cat){
+        array_push($output,$cat->name);
+    }
+    return(implode(' ',$output));
+};
+
+
+
+
+
+
+
 ?>
