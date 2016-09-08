3fffefa8d6b4d415a56a800154007ede2976ec62
John M Calhoun <johnmcalhoun123@gmail.com>
2016-09-07 21:58:23 -0500

    added post thumbnail load

--- js/src/test/js/renderThumbnail_test.js
+++ js/src/test/js/renderThumbnail_test.js
@@ -1,14 +1,47 @@
 goog.require('goog.testing.ContinuationTestCase')
 goog.require('goog.testing.jsunit')
+goog.require('goog.testing.PropertyReplacer')
 goog.require('bakehard.renderThumbnail')
-goog.require('goog.events.EventTarget')
 goog.require('goog.events.EventTarget')
 
 if(typeof renderThumbnail_test_flag != 'undefined'){
-var clear=function(){}
-var setup=clear
+var stubs;
 
-var testEmpty=function(){}
+var setUp=function(){
+    stubs=new goog.testing.PropertyReplacer();
+}
+
+var tearDown=function(){
+    stubs.reset();
+}
+var testEmpty=function(){
+    stubs.set(
+        bakehard.renderThumbnail,
+        "list_posts_url",
+        function(){
+            return("/src/test/json/post.json"); 
+        }
+    )
+     
+    var event_target=new goog.events.EventTarget();
+    goog.events.listenOnce( event_target,
+                            "check",
+                            function(){})
+    var triggered=false; 
+    waitForEvent(
+        event_target,
+        "check",
+        function(){           
+            assertNotEquals("data should be inserted",0,$('.card').length)
+        }
+    );
+    $(document).on("thumbnailLoaded",function(){
+        triggered=true;
+        event_target.dispatchEvent('check')
+    });
+  
+    bakehard.renderThumbnail.load()
+}
 
 var testCase=new goog.testing.ContinuationTestCase();
 testCase.autoDiscoverTests();
